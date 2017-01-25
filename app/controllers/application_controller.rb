01424ee9876846656322e9efff657d739abcbda7
Hanzolskl <Hanzolskl@gmail.com>
2017-01-25 18:10:48 +0300

    forgot one string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
+  include Pundit
   respond_to :html
   protect_from_forgery with: :exception
 end
