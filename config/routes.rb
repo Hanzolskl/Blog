5b220939e49fec70b20f2af0220fdc6683b5d21b
Hanzolskl <Hanzolskl@gmail.com>
2017-04-13 21:59:36 +0300

    Create layout header with sign in and sign out (#19)
    * #19 Create_layout_header_with_sign_in_and_sign_out
    
    * it's max.What i could do

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  root 'posts#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
